84d658dcb7be5e1e4b4cdc6cfe517e54c76be7fb
eddndev <contacto@eddndev.com>
2026-08-10 20:54:32 -0600

    test(layout): tighten prepared trait guard

--- crates/fenestra-ui-layout/tests/prepared_layout.rs
+++ crates/fenestra-ui-layout/tests/prepared_layout.rs
@@ -335,7 +335,7 @@ trait AmbiguousIfClone<A> {
 }
 
 impl<T: ?Sized> AmbiguousIfClone<()> for T {}
-impl<T: ?Sized + Clone> AmbiguousIfClone<u8> for T {}
+impl<T: Clone> AmbiguousIfClone<u8> for T {}
 
 trait AmbiguousIfDebug<A> {
     fn marker() {}
